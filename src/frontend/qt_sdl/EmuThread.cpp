ebc0886ead1093238af720d109045a3680dc8cc2
Antoine Giraud <antoine.giraud@advania.is>
2024-08-13 20:12:25 +0000

    test frameadvance1

--- src/frontend/qt_sdl/EmuThread.cpp
+++ src/frontend/qt_sdl/EmuThread.cpp
@@ -901,9 +901,11 @@ void EmuThread::run()
                 
                 NDS->ARM9Write8(0x020DB45B,11);
                 NDS->ARM9Write8(0x020DB463,2);
-                //NDS->ReleaseScreen();
-                frameAdvance(2);
                 NDS->ReleaseScreen();
+                frameAdvance(1);
+                NDS->ReleaseScreen();
+
+                
                 //frameAdvance(2);
                 //NDS->ReleaseScreen();
                 //frameAdvance(2);
