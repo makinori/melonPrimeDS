de7f41e475f2c9cb4f08fbf632bb99885df9efcc
Antoine Giraud <antoine.giraud@advania.is>
2024-08-13 19:10:55 +0000

    fix

--- src/frontend/qt_sdl/EmuThread.cpp
+++ src/frontend/qt_sdl/EmuThread.cpp
@@ -898,8 +898,8 @@ void EmuThread::run()
             // switch to missiles
             if (Input::HotkeyPressed(HK_MetroidWeaponMissile)) {
                 
-                NDS->ARM9Write32(0x02DB45B,11);
-                NDS->ARM9Write32(0x02DB45B,4);
+                NDS->ARM9Write32(0x020DB45B,11);
+                NDS->ARM9Write32(0x020DB45B,4);
                 //NDS->ReleaseScreen();
                 //frameAdvance(2);
                 //NDS->TouchScreen(85 + 40 * 1, 32);
